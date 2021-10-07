42a926f95411ce3b077a6ce91e06594ece2ed77a
Philip Helger <philip@helger.com>
2021-10-08 00:00:16 +0200

    Updated license headers

--- en16931-cii2ubl-cli/_test.cmd
+++ en16931-cii2ubl-cli/_test.cmd
@@ -1,3 +1,21 @@
+@REM
+@REM Copyright (C) 2019-2021 Philip Helger
+@REM http://www.helger.com
+@REM philip[at]helger[dot]com
+@REM
+@REM Licensed under the Apache License, Version 2.0 (the "License");
+@REM you may not use this file except in compliance with the License.
+@REM You may obtain a copy of the License at
+@REM
+@REM         http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS,
+@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM See the License for the specific language governing permissions and
+@REM limitations under the License.
+@REM
+
 @echo off
 java -jar target\en16931-cii2ubl-cli%V%-full.jar  src\test\resources\CII_example1.xml
 java -jar target\en16931-cii2ubl-cli%V%-full.jar  src\test\resources\not-existing.xml
--- en16931-cii2ubl/src/test/java/com/helger/en16931/cii2ubl/CIIToUBLVersionTest.java
+++ en16931-cii2ubl/src/test/java/com/helger/en16931/cii2ubl/CIIToUBLVersionTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 2019-2021 Philip Helger
  * http://www.helger.com
  * philip[at]helger[dot]com
