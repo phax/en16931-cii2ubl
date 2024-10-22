3ee0dfe5448b7fd954f1e2db20925234e3efe099
Torsten <thpost@gmx.net>
2024-10-22 17:04:55 +0200

    Support Java Modularity
    * Introduce `module-info.java` file to avoid problems with illegal module name 'en16931-cii2ubl'
    * Git ignore IDEA IDE configuration files

---
+++ en16931-cii2ubl-cli/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module ph.cii2ubl.cli {
+    requires info.picocli;
+    requires ph.cii2ubl;
+    requires jsr305;
+    requires org.slf4j;
+    requires com.helger.commons;
+    requires com.helger.ubl21;
+    requires com.helger.ubl22;
+    requires com.helger.ubl23;
+}
---
+++ en16931-cii2ubl/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module ph.cii2ubl {
+    exports com.helger.en16931.cii2ubl;
+    requires com.helger.cii.d16b;
+    requires com.helger.commons;
+    requires com.helger.jaxb;
+    requires com.helger.ubl21;
+    requires com.helger.ubl22;
+    requires com.helger.ubl23;
+    requires com.helger.ubl24;
+    requires jsr305;
+    requires org.slf4j;
+    requires com.helger.xsds.ccts.cct.schemamodule;
+    requires java.xml;
+}
